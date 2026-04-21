04ae20047ef8e4a830ae6d98a21074c3bde774f0
abogazee515-droid <abogazee515@gmail.com>
2026-04-21 10:17:48 +0700

    add Address struct

---
+++ meeting 8/meeting 8 Assignment 2.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+struct student {
+
+	string NIM;
+	string name;
+	string Address;
+	int Age;
+
+};
+
+
+
+int main() {
+
+	student ST;
+
+	cout << " please enter NIM ";
+	cin >> ST.NIM;
+
+	cout << endl;
+	cout << " please enter your name : ";
+	cin >> ST.name;
+
+	cout << endl;
+	cout << " please enter your Address :";
+	cin >> ST.Address;
+	cout << endl;
+
+	cout << " please enter your Age:";
+	cin >> ST.Age;
+	cout << endl;
+
+	cout << " NIM : " << ST.NIM << endl;
+	cout << " Name :" << ST.name << endl;
+	cout << " Address :" << ST.Address << endl;
+	cout << " Age : " << ST.Age;
+
+}
